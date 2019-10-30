13d838486d96994d9370c715287ed8870be5bad9
wuzhidejiekou <2621446103@qq.com>
2019-10-30 12:01:38 +0800

    周家敏提交代码

---
+++ SpringDemo/src/user/Iuser.java
@@ -0,0 +1,11 @@
+package user;
+
+import com.alibaba.druid.stat.TableStat.Name;
+
+public class Iuser {
+	public static String name="liliili";
+	public static void main(String[] args) {
+		System.out.println("名字"+name);
+		
+	}
+}
